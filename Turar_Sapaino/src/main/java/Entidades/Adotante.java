ca204e8659d25b518d2f81a85b4d8b9e93f62579
João Vitor Pereira Soareskkj <126785926+bito132@users.noreply.github.com>
2024-10-01 15:18:15 -0300

    Lo inicio de las mountanhas

---
+++ Turar_Sapaino/src/main/java/Entidades/Adotante.java
@@ -0,0 +1,9 @@
+package Entidades;
+
+
+public class Adotante extends Pessoa{
+
+    public Adotante(String CPF, String endereco, String nome, String dataDeNascimento, String email, String senha, String telefone) {
+        super(CPF, endereco, nome, dataDeNascimento, email, senha, telefone);
+    }
+}
